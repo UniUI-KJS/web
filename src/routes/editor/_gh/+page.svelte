563ff2adff2d0da49bf19c76bf2df2047d76b6a0
tizu69 <60812901+tizu69@users.noreply.github.com>
2024-03-17 22:59:46 +0100

    future me, was I high again? fuck this shit

--- src/routes/editor/_gh/+page.svelte
+++ src/routes/editor/_gh/+page.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-	import { base } from '$app/paths';
 	import { onMount } from 'svelte';
-	import Cookies from 'js-cookie';
-	import { fade } from 'svelte/transition';
 
 	onMount(() => {
 		window.opener.location?.reload();
@@ -12,13 +9,4 @@
 
 <div class="flex h-full items-center justify-center">
 	<h1 class="h1">Signed in!</h1>
-</div>
-
-<div class="absolute bottom-1 left-1 right-1 flex" in:fade|global={{ duration: 1, delay: 2000 }}>
-	<a
-		href="http://localhost:5173{base}/editor/_gh?token={Cookies.get('uniui-token')}"
-		class="variant-ghost-surface btn btn-sm mx-auto w-fit"
-	>
-		Use development server
-	</a>
 </div>
